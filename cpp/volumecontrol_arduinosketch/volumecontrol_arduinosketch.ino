7bd9b273825444e550e83f85cd160c7212a2698e
Alan Cruickshank <alanmcruickshank@gmail.com>
2018-03-03 14:35:16 +0000

    Arduino code works again!

--- cpp/volumecontrol_arduinosketch/volumecontrol_arduinosketch.ino
+++ cpp/volumecontrol_arduinosketch/volumecontrol_arduinosketch.ino
@@ -1,36 +1,37 @@
-#include <DigiKeyboard.h>
+#include "TrinketHidCombo.h"
+// Get the TrinketHidCombo from here: https://github.com/adafruit/Adafruit-Trinket-USB/tree/master/TrinketHidCombo
 
-#define KEY_VOL_UP       24  // 0x80
-#define KEY_VOL_DOWN     7 // 0x81
-#define KEY_PLAYPAUSE      39  // 0x48
-// could be 0xcd or 0x78
+#define ENCODER_PIN_A         2
+#define ENCODER_PIN_B         1
+#define LED_PIN               0
+#define BUTTON_PIN            5
 
-int out_d = 128;
-int targ_out_d = 24;
+#define SPIN_LED_IMPULSE      16
+#define STEADY_LED_LEVEL      36
+#define PLAYPAUSE_LED_LEVEL   220
+#define INIT_LED_LEVEL        220
+#define LED_REVERT_TIMEOUT    24
+
+int out_d = INIT_LED_LEVEL;
 int ps1 = 0;
 int ps2 = 0;
 int pm = 0;
 unsigned long bookmark_time;
-int ms_update_delay = 20;
-
-int ENCODER_PIN_A = 2;
-int ENCODER_PIN_B = 1;
-int LED_PIN = 0;
-int BUTTON_PIN = 5;
 
 // the setup routine runs once when you press reset:
-void setup() {                
+void setup() {
+  TrinketHidCombo.begin(); // start the USB device engine and enumerate     
   // initialize the digital pin as an output.
   pinMode(LED_PIN, OUTPUT); // LED Output
   pinMode(ENCODER_PIN_A, INPUT); // Encoder Pin 1
   pinMode(ENCODER_PIN_B, INPUT); // Encoder Pin 2
   pinMode(BUTTON_PIN, INPUT); // Pause Button
-  bookmark_time = millis() + ms_update_delay;
+  // initialise the first time at which we start to revert the LED
+  bookmark_time = millis() + LED_REVERT_TIMEOUT;
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  
   // Read the pins
   int a = digitalRead(ENCODER_PIN_A);
   int b = digitalRead(ENCODER_PIN_B);
@@ -42,19 +43,18 @@ void loop() {
     m = 1;
   }
 
-  
-
   // state is a number between 0 and 3, upper bit is pin 2, lower is pin 1
   int s = a + (b * 2);
 
   // has a state change occured with encoder?
   if (s != ps1) {
     if ((s == 2) && (ps1 == 3) && (ps2 == 1)) {
-      out_d += 8;
-      DigiKeyboard.sendKeyStroke(KEY_VOL_UP);
+      out_d += SPIN_LED_IMPULSE;
+      //StubbyVolume.sendKeyStroke(KEY_VOL_UP);
+      TrinketHidCombo.pressMultimediaKey(MMKEY_VOL_UP);
     } else if ((s == 1) && (ps1 == 3) && (ps2 == 2)) {
-      out_d -= 8;
-      DigiKeyboard.sendKeyStroke(KEY_VOL_DOWN);
+      out_d -= SPIN_LED_IMPULSE;
+      TrinketHidCombo.pressMultimediaKey(MMKEY_VOL_DOWN);
     }
 
     // make sure it doesn't overflow
@@ -76,26 +76,26 @@ void loop() {
     // is it a leading edge?
     if ((m == 1) && (pm == 0)) {
       // it is so press the key and boost the led
-      DigiKeyboard.sendKeyStroke(KEY_PLAYPAUSE);
-      out_d = 192;
+      TrinketHidCombo.pressMultimediaKey(MMKEY_PLAYPAUSE);
+      out_d = PLAYPAUSE_LED_LEVEL;
     }
     // store old state
     pm = m;
     // delay a little to debounce
-    DigiKeyboard.delay(5);
+    delay(5);
   } else if (millis() > bookmark_time) {
     // no state change occured, revert back towards standard intensity if enough time has elapsed
-    if (out_d < targ_out_d) {
+    if (out_d < STEADY_LED_LEVEL) {
       out_d += 1;
-    } else if (out_d > targ_out_d) {
+    } else if (out_d > STEADY_LED_LEVEL) {
       out_d -= 1;
     }
     // reset timer
-    bookmark_time = millis() + ms_update_delay;
+    bookmark_time = millis() + LED_REVERT_TIMEOUT;
     // write the value
     analogWrite(LED_PIN, out_d);
     // Send a polling stroke to remind the machine we're here
-    DigiKeyboard.sendKeyStroke(0);
+    TrinketHidCombo.poll(); // do nothing, check if USB needs anything done
   }
 }
 
