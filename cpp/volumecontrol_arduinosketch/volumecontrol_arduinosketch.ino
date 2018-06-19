80a878ae9ee6d16c77883ba04188dce62bdd3c3d
Alan Cruickshank <alanmcruickshank@gmail.com>
2018-06-19 18:19:50 +0100

    Refactor of firmware to fix the issue of not connecting until a button pressed. Also added the bump brightness feature so that brightness is a bit smoother.

--- cpp/volumecontrol_arduinosketch/volumecontrol_arduinosketch.ino
+++ cpp/volumecontrol_arduinosketch/volumecontrol_arduinosketch.ino
@@ -6,13 +6,15 @@
 #define LED_PIN               0
 #define BUTTON_PIN            5
 
-#define SPIN_LED_IMPULSE      16
-#define STEADY_LED_LEVEL      42
+#define SPIN_LED_IMPULSE      24
+#define STEADY_LED_LEVEL      52
 #define PLAYPAUSE_LED_LEVEL   220
 // Initial LED level
 #define INIT_LED_LEVEL        0
 // This is the timeout to move the LED level by 1
 #define LED_REVERT_TIMEOUT    28
+// This is the timeout to move the LED level by 1 (when in bump mode)
+#define BUMP_LED_REVERT_TIMEOUT    2
 #define DISCONN_LIMIT         200
 // The point at which pin five should be considered to be pulled low
 #define PIN_5_ANALOGUE_LIMIT  950
@@ -26,32 +28,28 @@
 
 byte led_set_point = 0;
 byte out_d = INIT_LED_LEVEL;
-unsigned long bookmark_time;
+// initialise the first time at which we start to revert the LED to zero so we force an update as soon as we're running
+unsigned long bookmark_time = 0;
 // initialise the buffer to keep track of track skipping (needs to be negative safe) (char is a signed 8 bit integer)
 char track_skip = 0;
 byte disconn_counter = 0;
+byte bump_target = 0;
+bool bump_active = false;
 
 // Using bit shifting for the buffer.
 // Encoder buffer is two bits at a time
 // Button buffer is one bit at a time
-byte encoder_state_buffer = B00000000;
-byte button_state_buffer = B00000000; 
+// Initialise the state buffers to zero
+byte encoder_state_buffer = 0b0;
+byte button_state_buffer = 0b0; 
 
 // the setup routine runs once when you press reset:
 void setup() {
-    TrinketHidCombo.begin(); // start the USB device engine and enumerate     
     // initialize the digital pin as an output.
     pinMode(LED_PIN, OUTPUT); // LED Output
     pinMode(ENCODER_PIN_A, INPUT); // Encoder Pin 1
     pinMode(ENCODER_PIN_B, INPUT); // Encoder Pin 2
     pinMode(BUTTON_PIN, INPUT); // Pause Button
-    // initialise the first time at which we start to revert the LED
-    bookmark_time = millis() + LED_REVERT_TIMEOUT;
-    // Initialise the state buffer
-    encoder_state_buffer = 0;
-    button_state_buffer = 0;
-    // delay a little to allow to usb enumeration
-    delay(5);
 }
 
 /* Read the current encoder state from the relevant pins */
@@ -84,22 +82,29 @@ byte capped_move(byte current_val, char increment){
     return constrain(val_buffer + increment, 0, 255);
 }
 
+/* reset the led timer */
+void reset_led_timer(){
+    if (bump_active) {
+       bookmark_time = millis() + BUMP_LED_REVERT_TIMEOUT;
+    } else {
+       bookmark_time = millis() + LED_REVERT_TIMEOUT;
+    }
+}
+
+/* use the led bump functionality to bump the brightness more nicely */
+void bump_output_to(byte target){
+    // bump the LED output to a given level
+    bump_target = target;
+    bump_active = true;
+    reset_led_timer();
+}
+
 // the loop routine runs over and over again forever:
 void loop() {
     // Get input states
     byte encoder_state = read_encoder_state();
     byte button_state = read_button_state();
     bool state_change = false;
-
-    // in any case update the pwm
-    //analogWrite(LED_PIN, out_d);
-
-    //if ((0b11010 & 0b11010) == 0b11010) {out_d = 255;}
-    //if (0b11111 & 0b11111 == 0b11111) {out_d = 128;}
-    //if ((encoder_state_buffer == 0b00000000) && (0b111 & 0b111 == 0b111)) {out_d = 255;}
-    //if (encoder_state == B00000011) {out_d = 255;}
-    
-    //if ((encoder_state == 0b11) && ((encoder_state_buffer & 0b111111) == 0b0)) {TrinketHidCombo.pressMultimediaKey(MMKEY_VOL_DOWN);}
 
     // ################## Case 1
     // Filter encoder buffer to see if it's changed (3 = 00000011 so masks to just the first two bits)
@@ -110,7 +115,7 @@ void loop() {
         // detect turns of the encoder
         if ((encoder_state_buffer & 0b111111) == EVENT_CLOCKWISE_TURN) {
             // if we can add the impulse without wraparound, then do so, otherwise cap
-            out_d = capped_move(out_d, SPIN_LED_IMPULSE);
+            bump_output_to(capped_move(out_d, SPIN_LED_IMPULSE));
             if (button_state) {
               // if the button is pressed then skip forward
               TrinketHidCombo.pressMultimediaKey(MMKEY_SCAN_NEXT_TRACK);
@@ -121,7 +126,7 @@ void loop() {
             }
         } else if ((encoder_state_buffer & 0b111111) == EVENT_ANTICLOCK_TURN) {
             // if we can take the impulse without wraparound, then do so, otherwise cap
-            out_d = capped_move(out_d, -SPIN_LED_IMPULSE);
+            bump_output_to(capped_move(out_d, -SPIN_LED_IMPULSE));
             if (button_state) {
               // if the button is pressed then skip forward
               TrinketHidCombo.pressMultimediaKey(MMKEY_SCAN_PREV_TRACK);
@@ -131,8 +136,6 @@ void loop() {
               TrinketHidCombo.pressMultimediaKey(MMKEY_VOL_DOWN);
             }
         }
-        // write the value
-        analogWrite(LED_PIN, out_d);
     }
     // ################## Case 2
     // Filter butten buffer to see if it's changed (1 = 00000001 so masks to just the first bit)
@@ -142,31 +145,44 @@ void loop() {
         if (button_state){
             // push
             // ... so boost the led and reset track skip
-            out_d = PLAYPAUSE_LED_LEVEL;
+            bump_output_to(PLAYPAUSE_LED_LEVEL);
             // reset the track buffer
             track_skip = 0;
         } else if (track_skip == 0) {
             // release (and no track skipping) - so trigger a play/pause
             TrinketHidCombo.pressMultimediaKey(MMKEY_PLAYPAUSE);
         }
-        // write the value
-        analogWrite(LED_PIN, out_d);
         // delay a little to debounce
         delay(2);
     // ################## Case 3
     } else if (millis() > bookmark_time) {
         // No state change of buttons and the timeout on the fading has been reached
-        if (out_d < led_set_point) {
-          out_d += 1;
-        } else if (out_d > led_set_point) {
-          out_d -= 1;
+        if (bump_active){
+            if (out_d < bump_target) {
+              out_d += 1;
+            } else if (out_d > bump_target) {
+              out_d -= 1;
+            } else {
+              bump_active = false;
+            }
+        } else {
+            if (out_d < led_set_point) {
+              out_d += 1;
+            } else if (out_d > led_set_point) {
+              out_d -= 1;
+            }
         }
         // reset timer
-        bookmark_time = millis() + LED_REVERT_TIMEOUT;
+        reset_led_timer();
         // write the value
         analogWrite(LED_PIN, out_d);
         // Check for disconnect
-        if (TrinketHidCombo.isConnected() == 0){
+        if (TrinketHidCombo.isConnected()){
+            // We're connected so make sure that the lights are on.
+            led_set_point = STEADY_LED_LEVEL;
+            // We're connected so reset the counter and poll
+            disconn_counter = 0;   
+         } else {
             // Increment the disconnect counter (to count the cycles we're disconnected
             // NB: this is cycles within the bookmark loop - so it's a slower loop than the main loop
             disconn_counter += 1;
@@ -176,19 +192,13 @@ void loop() {
             if (disconn_counter > DISCONN_LIMIT) {
                 // Force re-enumeration
                 TrinketHidCombo.begin();
-                delay(20);
-                TrinketHidCombo.poll();
                 // Reset counter so that we don't try too often
                 disconn_counter = 0;
             }
-         } else {
-            // We're connected so make sure that the lights are on.
-            led_set_point = STEADY_LED_LEVEL;
-            // We're connected so reset the counter and poll
-            disconn_counter = 0;
-            // Send a polling stroke to remind the machine we're here (given we're already connected)
-            TrinketHidCombo.poll(); // do nothing, check if USB needs anything done
          }
+         // Send a polling stroke to remind the machine we're here (given we're already connected)
+         TrinketHidCombo.poll(); // do nothing, check if USB needs anything done  
+         //NB: Polling even when not yet connected - forces the connection to initiate after a .begin() command
     }
 }
 
